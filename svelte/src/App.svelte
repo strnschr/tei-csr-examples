e45f532dbb3c9cb7f0b57b933f71bae6682b0195
strnschr <robin2go_no1@yahoo.de>
2023-04-14 19:00:16 +0200

    add svelte tei

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,6 +1,30 @@
+
+
 <script lang="ts">
+  import CETEI from 'CETEIcean';
+
+  const tei = new CETEI();
+  const urls = {
+    sturm:
+      'https://raw.githubusercontent.com/digicademy/sturm-exist-app/master/xml/quellen/01.briefe/1914-00-00/JVH/Q.01.19140322.JVH.01.xml',
+    faustus:
+      'https://raw.githubusercontent.com/evt-project/evt-sample-documents/master/EVT2js/data/text/marlowe.xml',
+  }
+ 
+  let editionElement;
+
+  function onClick(mode: 'sturm' | 'faustus') {
+    tei.getHTML5(urls[mode], (data) => {
+      editionElement.innerHTML = '';
+      editionElement.appendChild(data);
+    });
+  }
 </script>
 
 <main>
+  <button on:click={() => onClick('sturm')}>Der Sturm</button>
+  <button on:click={() => onClick('faustus')}>Faustus</button>
+
+  <div bind:this={editionElement}></div>
 </main>
 
