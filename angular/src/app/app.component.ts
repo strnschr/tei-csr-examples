b95f9d6cb8c546961edbebcdb10b4b3207fa9c8a
strnschr <robin2go_no1@yahoo.de>
2023-04-14 16:44:02 +0200

    added edition rendering to angular

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import CETEI from 'CETEIcean';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tei-csr';
+  private tei = new CETEI();
+
+  private urls = {
+    sturm:
+      'https://raw.githubusercontent.com/digicademy/sturm-exist-app/master/xml/quellen/01.briefe/1914-00-00/JVH/Q.01.19140322.JVH.01.xml',
+    faustus:
+      'https://raw.githubusercontent.com/evt-project/evt-sample-documents/master/EVT2js/data/text/marlowe.xml',
+  };
+
+  @ViewChild('edition')
+  selectedEdition: any;
+
+  onClick(mode: 'sturm' | 'faustus') {
+    this.tei.getHTML5(this.urls[mode], (data) => {
+      this.selectedEdition.nativeElement.innerHTML = '';
+      this.selectedEdition.nativeElement.appendChild(data);
+    });
+  }
 }
