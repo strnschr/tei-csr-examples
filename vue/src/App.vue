9c06e4de6b09588a6903a69020801027e9c45399
strnschr <robin2go_no1@yahoo.de>
2023-04-15 13:38:59 +0200

    add vue tei

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CETEI from 'CETEIcean'
+</script>
+
+<script lang="ts">
+const tei = new CETEI()
+const urls = {
+  sturm:
+    'https://raw.githubusercontent.com/digicademy/sturm-exist-app/master/xml/quellen/01.briefe/1914-00-00/JVH/Q.01.19140322.JVH.01.xml',
+  faustus:
+    'https://raw.githubusercontent.com/evt-project/evt-sample-documents/master/EVT2js/data/text/marlowe.xml'
+}
+
+let editionElement: any
+
+function onClick(mode: 'sturm' | 'faustus') {
+  tei.getHTML5(urls[mode], (data) => {
+    editionElement.innerHTML = ''
+    editionElement.appendChild(data)
+  })
+}
+
+export default {
+  mounted() {
+    editionElement = this.$refs.editionElement
+  }
+}
+</script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
+  <button @click="() => onClick('sturm')">Der Sturm</button>
+  <button @click="() => onClick('faustus')">Faustus</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div ref="editionElement"></div>
 </template>
